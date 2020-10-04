c45cb1e9d57522224ba74dd8f829248bb9a122ef
Isabel Li <isabel.yueli@gmail.com>
2020-10-04 13:20:11 +1300

    Updates

---
+++ precache-manifest.fc4a8b3a851157872f787e4babd4712f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b3fd59c95ffd3bb5b76918f105c9f9",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "8f0085d1f3fcd1754d77",
+    "url": "/personal-website/static/css/main.76d96e7f.chunk.css"
+  },
+  {
+    "revision": "ef450c8d801947cb6c29",
+    "url": "/personal-website/static/js/2.9c5ddeac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-website/static/js/2.9c5ddeac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f0085d1f3fcd1754d77",
+    "url": "/personal-website/static/js/main.eae0cffc.chunk.js"
+  },
+  {
+    "revision": "1ddb71d2aac6cc902664",
+    "url": "/personal-website/static/js/runtime-main.84b866af.js"
+  },
+  {
+    "revision": "9296916b5c7e2300b78db91a3978bcaa",
+    "url": "/personal-website/static/media/circle.9296916b.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal-website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1bb09bf728f99ef62697fe5b1556d98f",
+    "url": "/personal-website/static/media/proj1.1bb09bf7.png"
+  },
+  {
+    "revision": "3ec3a9321bb991a9834c5045d4d6b3b9",
+    "url": "/personal-website/static/media/proj2.3ec3a932.png"
+  },
+  {
+    "revision": "4b32dcf13839a57654e6d7f877d436f7",
+    "url": "/personal-website/static/media/proj3.4b32dcf1.png"
+  }
+]);
