99e804dc0861a8c9f5be93b765339642744902a3
Isabel Li <isabel.yueli@gmail.com>
2021-01-07 17:22:59 +1300

    Updates

---
+++ precache-manifest.2c22e8b220df52d7aef3fd0c424d6eff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c425c511e14e57c161f4bafc8ac80b",
+    "url": "/personal-site/index.html"
+  },
+  {
+    "revision": "9dd3555d47a3a74a4fd6",
+    "url": "/personal-site/static/css/main.d0ae1036.chunk.css"
+  },
+  {
+    "revision": "830d8ce14cfda7a88d87",
+    "url": "/personal-site/static/js/2.faeebc5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-site/static/js/2.faeebc5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dd3555d47a3a74a4fd6",
+    "url": "/personal-site/static/js/main.46fe9ac7.chunk.js"
+  },
+  {
+    "revision": "03d6741f00ad966597cc",
+    "url": "/personal-site/static/js/runtime-main.400e3341.js"
+  },
+  {
+    "revision": "9296916b5c7e2300b78db91a3978bcaa",
+    "url": "/personal-site/static/media/circle.9296916b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.348e21a7fb164add367d2af9767aa526.js"
+  "/personal-site/precache-manifest.2c22e8b220df52d7aef3fd0c424d6eff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-site/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
